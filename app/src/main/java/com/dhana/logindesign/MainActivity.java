a8ecec7e7a97b99296b37fa5d23c0ae8d1d9be8b
devpdhana <devpdhanasekar@gmail.com>
2022-10-24 09:43:33 +0530

    Android_Login_design User_Profile

--- app/src/main/java/com/dhana/logindesign/MainActivity.java
+++ app/src/main/java/com/dhana/logindesign/MainActivity.java
@@ -2,13 +2,61 @@ package com.dhana.logindesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    LinearLayout Login;
+    LinearLayout Signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.Tablayout);
+        Login = findViewById(R.id.LoginLayout);
+        Signup = findViewById(R.id.SignupLayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch(tab.getPosition()){
+                    case 0: {
+                        setVisible(Login, Signup);
+                        break;
+                    }
+                    case 1: {
+                        setVisible(Signup, Login);
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    private void setVisible(LinearLayout login, LinearLayout signup) {
+
+        login.setVisibility(View.VISIBLE);
+        signup.setVisibility(View.GONE);
     }
 }
